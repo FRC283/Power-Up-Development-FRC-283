a36bb641c61471aaae88b07d57acfd3129c529a6
FRC-283-PROG-2 <frcprogramming283.2@gmail.com>
2018-01-08 16:31:21 -0500

    Initial Commit of Base Robot File
    -Ben

---
+++ Power Up Robot Development/src/org/usfirst/frc/team283/robot/Robot.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team283.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Robot extends IterativeRobot 
+{
+	public void teleopPeriodic()
+	{
+		
+	}
+}
